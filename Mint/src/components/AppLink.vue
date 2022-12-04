c488315c6d8dd6ca2305b267ddb107a1ebbc80e1
Callum Cunningham <Callumcunningham@outlook.com>
2022-12-04 22:44:16 +0000

    Initial commit

---
+++ Mint/src/components/AppLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <a v-if="isExternalLink" v-bind="$attrs" :href="to" target="_blank" class="cursor-pointer">
+    <slot :active="false" />
+  </a>
+  <router-link
+    v-else
+    v-bind="$props"
+    custom
+    class="cursor-pointer">
+    <a
+      v-bind="$attrs"
+      :href="href"
+      @click="navigate"
+      :class="active ? activeClass : inactiveClass"
+    >
+      <slot :active="active" />
+    </a>
+  </router-link>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { RouterLink, useLink } from 'vue-router'
+
+const props = defineProps({
+  ...RouterLink.props,
+  inactiveClass: String,
+  exact: Boolean,
+})
+
+// `props` contains `to` and any other prop that can be passed to <router-link>
+const { navigate, href, route, isActive, isExactActive } = useLink(props)
+const isExternalLink = computed(() => typeof props.to === 'string' && props.to.startsWith('http'))
+const active = computed(() => (!props.exact && isActive.value) || isExactActive.value)
+</script>
---
+++ Mint/src/components/Header.vue
@@ -0,0 +1,29 @@
+<template>
+  <header class="py-10">
+    <nav class="relative z-50 flex justify-between">
+      <div class="flex items-center md:gap-x-12">
+        <div class="hidden lg:flex md:gap-x-6">
+          <AppLink v-for="(item, idx) in nav" :key="idx" :to="item.href"></AppLink>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+
+import AppLink from '../components/AppLink.vue'
+import { RouterLink, useLink } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const nav = [
+  {
+    name: 'Home',
+    href: { name: 'HomeView'},
+  },
+  {
+    name: 'About',
+    href: { name: 'AboutView'},
+  },
+]
+</script>
